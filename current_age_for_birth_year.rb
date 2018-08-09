703d34efb4d4ed89d6423ee03dd40c23de43025e
Milan Sacher <milan.lanlada@gmail.com>
2018-08-09 22:48:19 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,6 @@
 def current_age_for_birth_year(birth_year)
   current_year = 2003
-  current_year - current_age_for_birth_year
+  current_year - birth_year
 end 
 
 puts current_age_for_birth_year(1983)
